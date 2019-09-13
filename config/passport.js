770b68e3be5d4c7a44de31ca7751ec6f27328c0e
Haythem Trabelsi <trabelsi.haithem@gmail.com>
2019-09-13 17:18:45 +0100

    add jwt authentication with passport

--- config/passport.js
+++ config/passport.js
@@ -1,36 +1,42 @@
+require("dotenv").config();
 import passport from "passport";
-import LocalStrategy from "passport-local";
-import User from "../server/models/User";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+const { JWT_SECRET } = process.env;
+
+const cookieExtractor = function(req) {
+  var token = null;
+  if (req && req.cookies) {
+    token = req.cookies["jwt"];
+  }
+  return token;
+};
+
+const verifyOptions = {
+  jwtFromRequest: cookieExtractor,
+  secretOrKey: JWT_SECRET,
+  audience: "localhost",
+  algorithms: ["HS256"]
+};
 
 passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "user[email]",
-      passwordField: "user[password]"
-    },
-    (email, password, done) => {
-      User.findOne({ email })
-        .then(user => {
-          if (!user || !user.validatePassword(password)) {
-            return done(null, false, {
-              errors: { "email or password": "is invalid" }
-            });
-          }
-
-          return done(null, user);
-        })
-        .catch(done);
-    }
-  )
+  new JwtStrategy(verifyOptions, (payload, done) => done(null, payload))
 );
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 passport.deserializeUser((id, done) => {
-  const user = users.find(user => user.id === id);
+  const user = users.find(user => user._id === id);
   done(
     null,
-    user ? { id: user.id, username: user.username, name: user.name } : false
+    user ? { id: user._id, username: user.username, name: user.name } : false
   );
 });
+
+export const authenticate = (req, res) =>
+  new Promise((resolve, reject) => {
+    passport.authenticate("jwt", { session: false }, (err, payload) => {
+      if (err) reject(err);
+      resolve(payload);
+    })(req, res);
+  });
